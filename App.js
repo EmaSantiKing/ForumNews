282547fc46a18c22c2bf0f0100a9055fda879766
King <rey24vegas@gmail.com>
2025-05-09 22:10:33 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import PrimerComponente from './componentes/PrimerComponente';
+import SegundoComponente from './componentes/SegundoComponente';
+import TercerComponente from './componentes/TercerComponente';
+export default function App() {
+  return (
+    <>
+    
+    <PrimerComponente/>
+    <SegundoComponente/>
+    <TercerComponente/>
+    </>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
